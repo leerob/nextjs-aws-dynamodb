3b5139bed48fa7492a2652f05b59299d1103a890
Lee Robinson <lrobinson2011@gmail.com>
2022-08-28 19:09:39 -0500

    Move example.

--- bin/cdk.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-
-const cdk = require('@aws-cdk/core');
-const { DynamoStack } = require('../lib/cdk-stack');
-
-const app = new cdk.App();
-new DynamoStack(app, 'DynamoStack');
